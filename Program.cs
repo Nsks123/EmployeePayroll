895cc10b35411c97c935453136136c46505c6059
nallam sunilkumar <nn4399@srmist.edu.in>
2024-01-24 15:03:39 +0530

    [uc5] calculating_monthly_wages

--- Program.cs
+++ Program.cs
@@ -1,38 +1,45 @@
 ﻿using System;
 
-namespace EmployeePayroll
+namespace EWP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome To Employee Wage Management System");
             Random rnd = new Random();
 
-            while (true) 
+            int totalDays = 20;
+            int totalWages = 0;
+
+            for (int day = 1; day <= totalDays; day++)
             {
-                int attendance = rnd.Next(0, 2);
+                int workType = rnd.Next(0, 2);
 
-                if (attendance == 0)
+                switch (workType)
                 {
-                    Console.WriteLine("Present");
-
-                    int wagePerHour = 20;
-                    int fullDay = 8;
-                    int wagePerDay = wagePerHour * fullDay;
-                    Console.WriteLine($"Wage for a full day: {wagePerDay}");
+                    case 0:
+                        Console.WriteLine($"Day {day}: Full-time");
+                        totalWages += CalculateWages(20, 8);
+                        break;
+                    case 1:
+                        Console.WriteLine($"Day {day}: Part-time");
+                        totalWages += CalculateWages(20, 4);
+                        break;
+                    default:
+                        Console.WriteLine($"Day {day}: Invalid work type");
+                        break;
                 }
-                else
-                {
-                    Console.WriteLine("Absent");
-
-                    int wagePerHour = 20;
-                    int partTime = 4;
-                    int wagePerDay = wagePerHour * partTime;
-                    Console.WriteLine($"Wage for part-time: {wagePerDay}");
-                }
-
-                Console.ReadLine();
             }
+
+            Console.WriteLine($"Total wages for the month: {totalWages}");
+
+            Console.ReadLine();
+        }
+
+        static int CalculateWages(int wagePerHour, int hoursWorked)
+        {
+            return wagePerHour * hoursWorked;
         }
     }
 }
